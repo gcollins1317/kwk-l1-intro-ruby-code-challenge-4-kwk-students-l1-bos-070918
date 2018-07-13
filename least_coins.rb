01c7d1da91ac116ca76c5822d4816898a3ddaf44
gcollins1317 <gracecollins1000@gmail.com>
2018-07-13 13:39:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,26 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+while cents > 0 
+if cents >= 25 
+  quarter = (cents/25) 
+  cents = cents - (quarter * 25)
+elsif cents >=10 
+  dime = (cents/10)
+  cents = cents - (dime * 10)
+elsif cents >=5 
+  nickel = (cents/5) 
+  cents = cents - (nickel * 5)
+elsif cents >=1 
+  penny = cents
+end 
+end 
+puts coins = {
+"quarters" => quarter, 
+"dimes" => dime, 
+"nickels" => nickel,
+"pennies" => penny
+}
+return coins 
 end
+least_coins(29)
